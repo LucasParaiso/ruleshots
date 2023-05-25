67fb60c81cc664969f20bf39da2b8f62bfb63f0a
Lucas Paraiso <62018977+LucasParaiso@users.noreply.github.com>
2023-05-24 21:58:31 -0300

    User Table Changes

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,12 +17,8 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->string('adress')->nullable();
-            $table->string('phone')->nullable();
-            $table->string('social_media_1')->nullable();
-            $table->string('social_media_2')->nullable();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
+            $table->timestamp('email_verified_at')->nullable();
             $table->rememberToken();
             $table->timestamps();
         });
